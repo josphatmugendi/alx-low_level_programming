d23519180c52d6db60b77b7bdca2ced3ff1fb254
josphat josphat mugendi <josphatmugendi29@gmail.com>
2023-03-22 10:53:26 +0300

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+i#include <stdio.h>
 /**
  * main - entry point
  * Return: alway returns 0
@@ -7,15 +7,16 @@ int main(void)
 {
 	int i;
 	int j;
+
 	for (i = 10; i <= 19; i++)
 	{
-		for(j = 10; j <=19; j++)
+		for (j = 10; j <= 19; j++)
 		{
 			if ((j % 10) > (i % 10))
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
-				if (i !=18|| j != 19)
+				if (i != 18 || j != 19)
 				{
 					putchar(',');
 					putchar(' ');
@@ -25,5 +26,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}	
-					
+}

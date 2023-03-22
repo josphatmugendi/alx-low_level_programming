76d972ccd044f3e1b270ab8097a03c4d44f61cac
josphat josphat mugendi <josphatmugendi29@gmail.com>
2023-03-22 11:42:21 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *
+ * Return: always return 0
+ *
+ */
+
+int main(void)
+{
+	;
+
+	 srand(time(0));
+	 n = rand() - RAND_MAX / 2;
+	 /* your code goes there */
+	 return (0);
+}

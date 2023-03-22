f4cfbd05104e574251c6f2b38dffa852dccda71c
josphat josphat mugendi <josphatmugendi29@gmail.com>
2023-03-22 15:49:10 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+
+int main(void)
+{
+	_putchar('\_putchar');
+	_putchar('\n');
+         return 0;
+}
